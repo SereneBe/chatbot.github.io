4a1d149cc18fcdf7974488920e830a3c938f66c5
Kelsey Jackson-Porter <kelgp01@charter.net>
2018-07-05 10:23:44 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+document.getElementById("demo").style.fontSize = "35px";
+
+document.getElementById("demo").style.display = "none"
+
+document.getElementById("demo").style.display = "block";;
